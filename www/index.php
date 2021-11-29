e12bc9c7277f595b3d67af54ab09a0d4ae9c3b56
Athanasios Christou <thanoschri.it@gmail.com>
2021-11-29 23:00:42 +0200

    dbconnection update

--- www/index.php
+++ www/index.php
@@ -1,3 +1,19 @@
 <?php
+
+require_once "../lib/dbconnect.php";
+
+
 echo "This line from php...";
+
+$sql = "select * from cards";
+
+$st = $mysqli->prepare($sql);
+
+$st->execute();
+$res = $st->get_result();
+
+$r = $res->fetch_assoc();
+
+print "card_name: $r[cname], card_shape: $r[cshape]";
+
 ?>
